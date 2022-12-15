170efd4b4e72e9060fab87cbc73cc19aa2ff4b32
Rayza-Souto <rayzasouto@outlook.com>
2022-12-15 16:09:26 -0300

    Desafio de código 2-Avançando Tecnicamente Com a Expressão When

---
+++ src/main/kotlin/Desafio2.kt
@@ -0,0 +1,29 @@
+fun main() {
+    val entrada: String? = readLine()
+
+    val (dia, mes, ano) = entrada!!.split("/")
+
+    val mesTratado  = if(mes.substring(0) == "0"){
+        mes.substring(1)
+    }else{
+        mes
+    }
+
+    val mesPorExtenso = when (mesTratado.toInt()) {
+        1 -> "Janeiro"
+        2 -> "Fevereiro"
+        3 -> "Marco"
+        4 -> "Abril"
+        5 -> "Maio"
+        6 -> "Junho"
+        7 -> "Julho"
+        8 -> "Agosto"
+        9 -> "Setembro"
+        10 -> "Outubro"
+        11 -> "Novembro"
+        12 -> "Dezembro"
+        else -> "Mês Inválido!"
+    }
+
+    println("${dia} de ${mesPorExtenso} de ${ano}")
+}
